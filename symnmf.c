ca05e79983666c34ea22d2af5037e354099eab9b
root <nitsanhonen@mail.tau.ac.il>
2025-08-11 19:54:26 +0000

    finish parts 3&4

--- symnmf.c
+++ symnmf.c
@@ -8,8 +8,8 @@
 
 double *transpose_matrix(const double *mat, int n, int k){
     int i, j;
-    double *mat_t = malloc(n*k*sizeof(double));
-    if(mat_t == NULL) return NULL;
+    double *mat_t = malloc((size_t)n*k*sizeof(double));
+    if(!mat_t) return NULL;
     for(i=0; i<n; i++){
         for(j=0; j<k; j++){
             mat_t[(j*n)+i] = mat[(i*k)+j];
@@ -22,7 +22,7 @@ double *mat_mult(const double *matA, const double *matB, int rowsA, int cols_row
     int i, j, k;
     double sum;
     double *prod = malloc(rowsA * colsB * sizeof(double));
-    if(prod == NULL) return NULL;
+    if(!prod) return NULL;
     for(i=0; i<rowsA; i++){
         for(j=0; j<colsB; j++){
             sum=0.0;
@@ -35,7 +35,7 @@ double *mat_mult(const double *matA, const double *matB, int rowsA, int cols_row
     return prod;
 }
 
-int check_convergence(double *h_new, double *h_old, int n, int k){
+int check_convergence(const double *h_new, const double *h_old, int n, int k){
     int i, j;
     double sum = 0.0;
     for(i=0; i<n; i++){
@@ -46,42 +46,71 @@ int check_convergence(double *h_new, double *h_old, int n, int k){
     return sum < EPSILON;
 }
 
-double *create_h_new(double *old_h, double *w_matrix, int n, int k){
-    int i, j;
-    double inside;
-    double *h_matrix, *old_transpose, *mone, *mehane, *temp;
-    h_matrix = malloc(n*k*sizeof(double));
-    if(h_matrix == NULL){
-        free(old_h);
+double *mult_transpose(const double *mat, int n, int k){
+    double *transpose, *res, *temp;
+    transpose = transpose_matrix(mat, n, k);
+    if(!transpose) return NULL;
+    temp = mat_mult(transpose, mat, k, n, k);
+    if(!temp){
+        free(transpose);
         return NULL;
     }
-    old_transpose = transpose_matrix(old_h, n, k);
+    res = mat_mult(mat, temp, n, k, k);
+    free(transpose);
+    free(temp);
+    return res;
+}
+
+double *create_h_new(const double *old_h, const double *w_matrix, int n, int k){
+    int i, j;
+    double inside;
+    double *h_matrix, *mone, *mehane;
+    h_matrix = malloc((size_t)n*k*sizeof(double));
+    if(!h_matrix) return NULL;
     mone = mat_mult(w_matrix, old_h, n, n, k);
-    temp = mat_mult(old_h, old_transpose, n, k, n);
-    mehane = mat_mult(temp, old_h, n, n, k);
-    for(j=0, j<n; j++){
-        for(p=0; p<k; p++){
-            inside = (mone[(j*k) + p])/(mehane[(j*k) + p] + SMALL_NUMBER);
-            h_matrix[(j*k) + p] = old_h[(j*k) + p]*(BETA+(BETA*inside));
-        }
-        //continue!!!!!!!!!!!!
+    if(!mone){
+        free(h_matrix);
+        return NULL;
     }
+    mehane = mult_transpose(old_h, n, k);
+    if(!mehane){
+        free(h_matrix);
+        free(mone);
+        return NULL;
+    }
+    for(i=0; i<n; i++){
+        for(j=0; j<k; j++){
+            inside = (mone[(i*k) + j])/(mehane[(i*k) + j] + SMALL_NUMBER);
+            h_matrix[(i*k) + j] = old_h[(i*k) + j]*((1.0-BETA)+(BETA*inside));
+        }
+    }
+    free(mone);
+    free(mehane);
+    return h_matrix;
 }
 
 double *converge_h(double *h_matrix, const double *w_matrix, int n, int k){
-    int i;
+    int i, res;
     double *old_h;
     for(i=0; i<MAX_ITER; i++){
         old_h = h_matrix;
-        //continue!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    
+        h_matrix = create_h_new(old_h, w_matrix, n, k);
+        if(!h_matrix){
+            free(old_h);
+            return NULL;
+        }
+        res = check_convergence(h_matrix, old_h, n, k);
+        free(old_h);
+        if(res) break;
     }
+    return h_matrix;
 }
 
 double *build_w(const double *d_matrix, const double *a_matrix, int n){
     int i,j;
     double d_i, d_j;
-    double *w = malloc(n*n*sizeof(double));
-    if(w == NULL) return NULL;
+    double *w = malloc((size_t)n*n*sizeof(double));
+    if(!w) return NULL;
     for(i=0; i<n;i++){
         d_i = d_matrix[i] != 0.0 ? 1.0/sqrt(d_matrix[i]) : 0.0;
         for(j=0;j<n;j++){
